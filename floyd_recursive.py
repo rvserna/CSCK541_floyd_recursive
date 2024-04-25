9219e003c87f4414e2901fe4f1045f2b7bedb428
rvserna <viensmrobert@gmail.com>
2024-04-25 19:25:25 -0400

    Recursive Algorithm

--- floyd_recursive.py
+++ floyd_recursive.py
@@ -1,10 +1,11 @@
 """Recursive Version of Floyd Warshall Algorithm"""
-
-# Import sys module to define NO_PATH variable
+# Import the sys module to
+# define NO_PATH variable.
 import sys
-# Set NO_PATH equal to maximize value to represent infinity
+# Set 'NO_PATH' equal to the
+# maximum value to represent infinity.
 NO_PATH = sys.maxsize
-# Create new graph variable with same lists as example algorithm
+# Add graph from imperative algorithm.
 graph = [[0, 7, NO_PATH, 8],
         [NO_PATH, 0 , 5, NO_PATH],
         [NO_PATH, NO_PATH, 0, 2],
@@ -16,23 +17,27 @@ MAX_LENGTH = len(graph[0])
 def floyd_warshall(graph):
     """ Calculates shortest distance between all
         pairs of nodes in graph """
-    # Create nested function to find the minimum distance using recursion
+    # Create a nested function to
+    # find the minimum distance using
+    # recursion.
     def shortest_path(a, z):
-        # Create a base case where
-        # the distance will be zero
-        # because the start node and
-        # end node are the same
+        # Create a base case.
+        # When the start node and
+        # end node are the same,
+        # the distance will be zero.
         if a == z:
             return 0
-        # Calculate minimum distance using recursive calls
+        # Calculate the minimum distance using
+        # recursive calls.
         return min(
              shortest_path(a, z), shortest_path(a, m) + shortest_path(m, z))
-    # Add all vertices and update graph
+    # Add all vertices and update
+    # the graph.
     for a in range(MAX_LENGTH):
         for m in range(MAX_LENGTH):
             for z in range(MAX_LENGTH):
                 graph[a][z] = min(graph[a][z], graph[a][m] + graph[m][z])
     return graph
 
-# Print updated graph
+# Print the updated graph.
 print(floyd_warshall(graph))
